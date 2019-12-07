c8016d43b757027d5e9f043b3bca0b73ff0c321f
RaduEmanuel92 <chiscariuradu@gmail.com>
2019-12-07 17:53:51 +0200

    Added complete solution for Day 1.

--- p1/p1/src/main.rs
+++ p1/p1/src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     env,
     fs::File,
-    io::{prelude::*, Error,ErrorKind, BufReader},
+    io::{prelude::*, Error, ErrorKind, BufReader},
     path::Path
 };
 
@@ -18,31 +18,47 @@ fn modules_from_file(filename: impl AsRef<Path>) -> Result<Vec<i32>, Error>  {
     }
    
     Ok(module_masses) 
-  
 }
 
-fn fuel_requirements(module_masses: Vec<i32>)  -> i32 {
+fn compute_mass(mass: i32) -> i32 {
+    let fin_mass: i32 = mass/3 - 2;
     
+    fin_mass
+}
+
+fn fuel_partial_requirements(module_masses: &Vec<i32>)  -> i32 {
     let mut total: i32 = 0;
+
     for mass in module_masses {
-        total += mass/3 - 2;    
+        total += compute_mass(*mass);    
     }
 
    total
 }
 
+fn fuel_final_requirements(module_masses: &Vec<i32>)  -> i32 {
+    let mut total:   i32 = 0;
+    let mut partial: i32 = 0;
+
+    for mass in module_masses {
+        partial = compute_mass(*mass);
+        while partial > 0 {
+            total += partial;
+            partial = compute_mass(partial);
+        }
+    }
+
+   total
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let ship_components = &args[1];
-    
     let modules = modules_from_file(ship_components);
 
     for module in modules {
-        println!("{:?}", module);
-        let total_mass = fuel_requirements(module);
-        println!("[+] Total mass : {}", total_mass);
-    }
-
-    //
+        println!("[+] Parsing modules...");
+        println!("[+] 1. Partial quantity of fuel needed : {}", fuel_partial_requirements(&module));
+        println!("[+] 2. Total quantity of fuel needed : {}", fuel_final_requirements(&module));
+    }  
 } 
